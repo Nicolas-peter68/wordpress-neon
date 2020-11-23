f37802279ef8d5051ac51e367ef4291901f8add5
Nicolas-peter68 <71389477+Nicolas-peter68@users.noreply.github.com>
2020-11-23 15:01:34 +0100

    Initial commit

---
+++ wp-content/themes/Theme-neon/functions.php
@@ -0,0 +1,21 @@
+<?php 
+
+// Ajouter la prise en charge des images mises en avant
+add_theme_support( 'post-thumbnails' );
+
+// Ajouter automatiquement le titre du site dans l'en-tête du site
+add_theme_support( 'title-tag' );
+
+register_nav_menus( array(
+	'main' => 'Menu Principal',
+	'footer' => 'Bas de page',
+) );
+
+register_sidebar( array(
+    'id' => 'blog-sidebar',
+    'name' => 'Blog',
+    'before_widget'  => '<div class="site__sidebar__widget %2$s">',
+    'after_widget'  => '</div>',
+    'before_title' => '<p class="site__sidebar__widget__title">',
+    'after_title' => '</p>',
+  ) );
